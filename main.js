66f2249c86592b9e67731797be6b5c2e80bd3730
sdayra-afk <s.dayra@escola.pr.gov.br>
2026-05-18 09:27:20 -0300

    atulizaçao 18\05

---
+++ main.js
@@ -0,0 +1,12 @@
+const botoes = document.querySelectorAll(".botoes");
+
+for(let i=0;1<botoes.length;i++){
+    botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classlits.add("ativo");
+    }
+        
+}  
